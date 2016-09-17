85948b2846be981bcc9e89f52243e5804e85981f
Javier <jsm.multimedia@gmail.com>
2016-09-17 20:40:40 +0200

    API/REST

---
+++ slim/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+import {RestaurantesListComponent} from "./components/restaurantes-list.component";
+import {RestaurantesDetailComponent} from "./components/restaurantes-detail.component";
+import {RestauranteAddComponent} from "./components/restaurante-add.component";
+import {RestauranteEditComponent} from "./components/restaurante-edit.component";
+
+@Component({
+	selector: "mi-app",
+	templateUrl: "app/view/home.html",
+	directives: [RestaurantesListComponent, ROUTER_DIRECTIVES]
+})
+
+@RouteConfig([
+	{path: '/', name: "Home", component: RestaurantesListComponent, useAsDefault: true},
+	{path: "/restaurante/:id", name: "Restaurante", component: RestaurantesDetailComponent},
+	{path: "/crear-restaurante/", name: "CrearRestaurante", component: RestauranteAddComponent},
+	{path: "/editar-restaurante/:id", name: "EditarRestaurante", component: RestauranteEditComponent},
+	{path: "/donde-como-hoy/:random", name: "DondeComoHoy", component: RestaurantesDetailComponent},
+
+])
+
+export class AppComponent{
+	public titulo:string = "Restaurantes";
+
+
+}
